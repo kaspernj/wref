c6a30cabcf9ad98a2c9b2e964ba53feec38cbb4c
Kasper Johansen <k@spernj.org>
2012-05-10 11:02:48 +0200

    First release.

--- spec/wref_spec.rb
+++ spec/wref_spec.rb
@@ -1,7 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Wref" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should not fail" do
+    str = "Test"
+    ref = Wref.new(str)
+    raise "Should have been alive but wasnt." if !ref.alive?
+    str = nil
+    GC.start
+    raise "Should have been GCed but wasnt." if ref.alive?
+    
+    
+    str = "Test"
+    map = Wref_map.new
+    map[5] = str
+    raise "Should have been valid but wasnt." if !map.valid?(5)
+    str = nil
+    GC.start
+    raise "Should habe been garbage collected but wasnt." if !map.valid?(5)
   end
 end
